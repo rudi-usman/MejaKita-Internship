599e701741677a0db5ab3bc121096d8674e7337c
Rudi Usman <56495336+rudi-usman@users.noreply.github.com>
2021-02-10 21:32:54 +0700

    Add files via upload

---
+++ Phase_1/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="jumbotron jumbotron-fluid bg-success">
+        <div class="container">
+            <h1 class="display-4">Header</h1>
+            <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit exercitationem delectus vitae
+                at possimus. Consequatur accusantium excepturi cumque, repellat laudantium fugit ipsum alias nihil
+                perferendis harum explicabo nesciunt ipsam quo! <br> Lorem ipsum, dolor sit amet consectetur adipisicing
+                elit. Pariatur suscipit voluptatibus tempora similique sint veniam sunt exercitationem id deserunt
+                labore animi tenetur fuga dolorem laudantium enim, ullam eos praesentium aliquam!</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-5 bg-primary">col-5</div>
+            <div class="col"></div>
+            <div class="col-5 bg-warning">col-5</div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col bg-primary mt-4" style="text-align: center;">
+                Variable width content
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col bg-danger mt-5" style="text-align: center;">
+                <p>Content</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="footer">
+                <p>ini adalah Footer</p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
